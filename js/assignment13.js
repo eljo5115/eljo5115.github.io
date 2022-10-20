52ea8ade27181211afd1d05d9372bab3148f3079
Eli Jordan <eljo5115@colorado.edu>
2022-10-20 15:56:46 -0600

    Update assignment13.js

--- js/assignment13.js
+++ js/assignment13.js
@@ -12,15 +12,20 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-var xItem = randomValueFromArray(insertX);
-var yItem = randomValueFromArray(insertY);
-var zItem = randomValueFromArray(insertZ);
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 var newStory = storyText;
+
 randomize.addEventListener('click', result);
 
 function result() {
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
@@ -32,12 +37,10 @@ function result() {
     const temperature =  Math.round(94);
     const stone = weight/14;
     const centigrade = (temperature-32) *5/9;
-    newStory = newStory.replace("94 fahrenheit", Math.floor(centigrade) +" Celsius");
+    newStory = newStory.replace("94 fahrenheit", Math.floor(centigrade) + " Celsius");
     newStory = newStory.replace("300 pounds", Math.floor(stone) + " stone");
   }
-newStory = newStory.replace(/:insertx:/g, xItem);
-newStory = newStory.replace(":inserty:", yItem);
-newStory = newStory.replace(":insertz:", zItem);
+
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
