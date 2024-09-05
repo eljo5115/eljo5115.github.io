a28d2bfa78ac5f99d1a5e1b09df63fe6518f1d71
Eli Jordan <89488311+eljo5115@users.noreply.github.com>
2024-09-05 15:55:16 -0600

    updated 3index

--- js/threeScript.js
+++ js/threeScript.js
@@ -4,34 +4,41 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 
-const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.getElementById("three-canvas");
+const renderer = new THREE.WebGLRenderer({antialias:true,canvas:canvas});
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xfdfdfd );
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
+// document.body.appendChild( renderer.domElement );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+function drawObjects(){
 
-const pointLight = new THREE.PointLight(0xFFFFFF);
-pointLight.position.set( 5, 5, 5 );
-const ambientLight = new THREE.AmbientLight(0xFFFFFF);
-scene.add( pointLight,ambientLight );
-
-
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    
+    const pointLight = new THREE.PointLight(0xFFFFFF);
+    pointLight.position.set( 5, 5, 5 );
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+    scene.add( pointLight,ambientLight );
+}
 
 camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame( animate );
+function animateCube(){
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+    animateCube();
 	renderer.render( scene, camera );
 }
 
@@ -41,5 +48,6 @@ function drawHelpers()
     const gridHelper = new THREE.GridHelper(200,50);
     scene.add( gridHelper,pointLightHelper );
 }
+drawObjects();
 drawHelpers();
 animate();
