5fb7d27709023c5f1ae51db97413935ffcce603c
Eli Jordan <89488311+eljo5115@users.noreply.github.com>
2024-09-05 17:02:18 -0600

    Update threeScript.js

--- js/threeScript.js
+++ js/threeScript.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { Clock } from 'three';
 
 
 
@@ -16,19 +17,34 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 
-function drawObjects(){
 
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    
-    const pointLight = new THREE.PointLight(0xFFFFFF);
-    pointLight.position.set( 5, 5, 5 );
-    const ambientLight = new THREE.AmbientLight(0xFFFFFF);
-    scene.add( pointLight,ambientLight );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set( 5, 5, 5 );
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+scene.add( pointLight,ambientLight );
+
+const icosaGeo = new THREE.IcosahedronGeometry(1.5);
+const icosaMat = new THREE.MeshDepthMaterial({
+    color:0x5f5f5f
+    ,transparent: true
+    ,opacity:0.1
+});
+const icosahedron = new THREE.Mesh(icosaGeo, icosaMat);
+scene.add (icosahedron);
+
+var iy = 0
+function animateIcosahedron(){
+    iy+=0.05;
+    icosahedron.position.y = (Math.sin(iy) * 0.15) + 0.5;
+    icosahedron.rotation.x +=0.01;
+    icosahedron.rotation.y +=0.01;
 }
-
 camera.position.z = 5;
 
 function animateCube(){
@@ -36,9 +52,12 @@ function animateCube(){
     cube.rotation.y += 0.01;
 }
 
+
+
 function animate() {
 	requestAnimationFrame( animate );
     animateCube();
+    animateIcosahedron();
 	renderer.render( scene, camera );
 }
 
@@ -48,6 +67,6 @@ function drawHelpers()
     const gridHelper = new THREE.GridHelper(200,50);
     scene.add( gridHelper,pointLightHelper );
 }
-drawObjects();
+
 drawHelpers();
 animate();
